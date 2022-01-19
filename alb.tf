0233200b186cce9f9d64f70d24e1c3eceb78eef2
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-19 06:23:58 +0530

    Update terrafrom-mutable-alb/

--- alb.tf
+++ alb.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "public" {
   name               = "${var.ENV}-public-alb"
   internal           = false
   load_balancer_type = "application"
-  //security_groups    = [aws_security_group.lb_sg.id]
-  subnets = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS
+  security_groups    = [aws_security_group.public_lb.id]
+  subnets            = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS
 
   tags = {
     Environment = "${var.ENV}-public-alb"
@@ -14,8 +14,8 @@ resource "aws_lb" "private" {
   name               = "${var.ENV}-private-alb"
   internal           = false
   load_balancer_type = "application"
-  //security_groups    = [aws_security_group.lb_sg.id]
-  subnets = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+  security_groups    = [aws_security_group.private_lb.id]
+  subnets            = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
 
   tags = {
     Environment = "${var.ENV}-private-alb"
